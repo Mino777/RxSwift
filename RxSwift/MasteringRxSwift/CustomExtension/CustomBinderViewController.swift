b0f022784b88e6d926fea632cc14140424b25e3c
Minho Jo <jomino7777@gmail.com>
2021-03-30 11:41:06 +0900

    add: RxSwift Lecture #Custom Binder

--- RxSwift/MasteringRxSwift/CustomExtension/CustomBinderViewController.swift
+++ RxSwift/MasteringRxSwift/CustomExtension/CustomBinderViewController.swift
@@ -25,52 +25,77 @@ import RxSwift
 import RxCocoa
 
 class CustomBinderViewController: UIViewController {
-   
-   let bag = DisposeBag()
-   
-   @IBOutlet weak var valueLabel: UILabel!
-   
-   @IBOutlet weak var colorPicker: UISegmentedControl!
-   
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      
-      colorPicker.rx.selectedSegmentIndex
-         .map { index -> UIColor in
-            switch index {
-            case 0:
-               return UIColor.red
-            case 1:
-               return UIColor.green
-            case 2:
-               return UIColor.blue
-            default:
-               return UIColor.black
-            }
-         }
-         .subscribe(onNext: { [weak self] color in
-            self?.valueLabel.textColor = color
-         })
-         .disposed(by: bag)
-
-      colorPicker.rx.selectedSegmentIndex
-         .map { index -> String? in
-            switch index {
-            case 0:
-               return "Red"
-            case 1:
-               return "Green"
-            case 2:
-               return "Blue"
-            default:
-               return "Unknown"
-            }
-         }
-         .bind(to: valueLabel.rx.text)
-         .disposed(by: bag) 
-   }
+    
+    let bag = DisposeBag()
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBOutlet weak var colorPicker: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //      colorPicker.rx.selectedSegmentIndex
+        //         .map { index -> UIColor in
+        //            switch index {
+        //            case 0:
+        //               return UIColor.red
+        //            case 1:
+        //               return UIColor.green
+        //            case 2:
+        //               return UIColor.blue
+        //            default:
+        //               return UIColor.black
+        //            }
+        //         }
+        //         .subscribe(onNext: { [weak self] color in
+        //            self?.valueLabel.textColor = color
+        //         })
+        //         .disposed(by: bag)
+        //
+        //      colorPicker.rx.selectedSegmentIndex
+        //         .map { index -> String? in
+        //            switch index {
+        //            case 0:
+        //               return "Red"
+        //            case 1:
+        //               return "Green"
+        //            case 2:
+        //               return "Blue"
+        //            default:
+        //               return "Unknown"
+        //            }
+        //         }
+        //         .bind(to: valueLabel.rx.text)
+        //         .disposed(by: bag)
+        
+        // 코드가 굉장히 단순하고 직관적!
+        colorPicker.rx.selectedSegmentIndex
+            .bind(to: valueLabel.rx.segmentedValue)
+            .disposed(by: DisposeBag())
+    }
 }
 
+extension Reactive where Base: UILabel {
+    var segmentedValue: Binder<Int> {
+        return Binder(self.base) { label, index in
+            switch index {
+            case 0:
+                label.text = "Red"
+                label.textColor = UIColor.red
+            case 1:
+                label.text = "Green"
+                label.textColor = UIColor.green
+            case 2:
+                label.text = "Blue"
+                label.textColor = UIColor.blue
+            default:
+                label.text = "Unknown"
+                label.textColor = UIColor.black
+            }
+        }
+    }
+}
 
 
