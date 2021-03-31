bd1b75f27540c55b68f0d30197cda98f0c4ec41e
Minho Jo <jomino7777@gmail.com>
2021-03-31 11:12:47 +0900

    add: RxSwift Lecture #Scheduler

--- RxSwift/Scheduler/Scheduler.playground/Contents.swift
+++ RxSwift/Scheduler/Scheduler.playground/Contents.swift
@@ -26,20 +26,39 @@ import RxSwift
 /*:
  # Scheduler
  */
+// Main Queue == Main Scheduler
+// Global Queue == Background Scheduler
 
+// Serial
+// -> CurrentThreadScheduler, MainScheduler, SerialDispatchQueueScheduler
+
+// Concurrent Scheduler
+// -> ConcurrentDispatchQueueScheduler, OpertationQueueScheduler
+
+// + TestScheduler, CustomScheduler
+
+// subscribeOn은 옵져버블이 시작되는 스케쥴러를 지정
+// observeOn은 이어지는 연산자가 시작되는 스케쥴러를 지정
 let bag = DisposeBag()
 
+let backgroundScheduler = ConcurrentDispatchQueueScheduler(queue: DispatchQueue.global())
+
 Observable.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
-   .filter { num -> Bool in
-      print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> filter")
-      return num.isMultiple(of: 2)
-   }
-   .map { num -> Int in
-      print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> map")
-      return num * 2
-   }
-
-
+    .subscribeOn(MainScheduler.instance) // subscribeOn에 위치는 상관없음.
+    .filter { num -> Bool in
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> filter")
+        return num.isMultiple(of: 2)
+    }
+    .observeOn(backgroundScheduler) // observeOn의 위치는 중요함.
+    .map { num -> Int in
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> map")
+        return num * 2
+    }
+    .observeOn(MainScheduler.instance)
+    .subscribe {
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> subscribe")
+        print($0)
+    }.disposed(by: bag)
 
 
 
