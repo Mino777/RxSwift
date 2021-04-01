b31da64d000dbe971ac37f210a50d213ba8a1052
Minho Jo <jomino7777@gmail.com>
2021-04-01 11:02:08 +0900

    add: RxWeather Lecture #2 Weather API

--- RxWeather/RxWeather/Service/WeatherApi.swift
+++ RxWeather/RxWeather/Service/WeatherApi.swift
@@ -25,3 +25,55 @@ import RxSwift
 import RxCocoa
 import CoreLocation
 import NSObject_Rx
+
+
+class OpenWeatherMapApi: NSObject, WeatherApiType {
+    // 현재 날씨 방출, UI Binding에 활용되기 때문에 BehaviorRelay로 선언
+    private let summaryRelay = BehaviorRelay<WeatherDataType?>(value: nil)
+    
+    private let forecastRelay = BehaviorRelay<[WeatherDataType]>(value: [])
+    
+    // Network 속성
+    private let urlSession = URLSession.shared
+    
+    // 현재 날씨 가져오기
+    private func fetchSummary(location: CLLocation) -> Observable<WeatherDataType?> {
+        let request = composeUrlRequest(endpoint: summaryEndpoint, from: location)
+        
+        return urlSession.rx.data(request: request)
+            .map { data -> WeatherSummary in
+                let decoder = JSONDecoder()
+                return try decoder.decode(WeatherSummary.self, from: data)
+            }
+            .map { WeatherData(summary: $0) }
+            .catchErrorJustReturn(nil)
+    }
+    
+    // 예보 데이터
+    private func fetchForecast(location: CLLocation) -> Observable<[WeatherDataType]> {
+        let request = composeUrlRequest(endpoint: forecastEndpoint, from: location)
+        
+        return urlSession.rx.data(request: request)
+            .map { data -> [WeatherData] in
+                let decoder = JSONDecoder()
+                let forecast = try decoder.decode(Forecast.self, from: data)
+                
+                return forecast.list.map(WeatherData.init)
+            }
+            .catchErrorJustReturn([])
+    }
+    
+    @discardableResult
+    func fetch(location: CLLocation) -> Observable<(WeatherDataType?, [WeatherDataType])> {
+        let summary = fetchSummary(location: location)
+        let forecast = fetchForecast(location: location)
+        
+        Observable.zip(summary, forecast)
+            .subscribe(onNext: { [weak self] result in
+                self?.summaryRelay.accept(result.0)
+                self?.forecastRelay.accept(result.1)
+            }).disposed(by: rx.disposeBag)
+        
+        return Observable.combineLatest(summaryRelay.asObservable(), forecastRelay.asObservable())
+    }
+}
